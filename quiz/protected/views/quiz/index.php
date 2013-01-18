cdd75e1731d94d2be97f64a8ce535f6534f6c010
Agatha Antipova <agatha.antipova@gmail.com>
2013-01-18 11:50:31 +0400

    Feature re #quiz : additional commit

--- quiz/protected/views/quiz/index.php
+++ quiz/protected/views/quiz/index.php
@@ -2,12 +2,7 @@
 /* @var $this QuizController */
 
 ?>
-<h1><?php echo $this->id . '/' . $this->action->id ?></h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <tt><?php echo __FILE__; ?></tt>.
-</p>
 <form id="page-content" action="#">
     <?php if ($question) {
         $this->renderPartial('question', array('question' => $question));

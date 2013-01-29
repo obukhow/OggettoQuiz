34c25d990ef8bbfee9bf4aad0937fa6dcd20c5b6
Denis Obukhov <denis.obukhov@oggettoweb.com>
2013-01-29 16:22:22 +0400

    re #7747 Refactoring

--- quiz/protected/controllers/SiteController.php
+++ quiz/protected/controllers/SiteController.php
@@ -2,120 +2,103 @@
 
 class SiteController extends Controller
 {
-	/**
-	 * Declares class-based actions.
-	 */
-	public function actions()
-	{
-		return array(
-			// captcha action renders the CAPTCHA image displayed on the contact page
-			'captcha'=>array(
-				'class'=>'CCaptchaAction',
-				'backColor'=>0xFFFFFF,
-			),
-			// page action renders "static" pages stored under 'protected/views/site/pages'
-			// They can be accessed via: index.php?r=site/page&view=FileName
-			'page'=>array(
-				'class'=>'CViewAction',
-			),
-		);
-	}
+    /**
+     * Declares class-based actions.
+     * 
+     * @return array
+     */
+    public function actions()
+    {
+        return array(
+            // captcha action renders the CAPTCHA image displayed on the contact page
+            'captcha' => array(
+                'class'     => 'CCaptchaAction',
+                'backColor' => 0xFFFFFF,
+            ),
+            // page action renders "static" pages stored under 'protected/views/site/pages'
+            // They can be accessed via: index.php?r=site/page&view=FileName
+            'page' => array(
+                'class' => 'CViewAction',
+            ),
+        );
+    }
 
-	/**
-	 * This is the default 'index' action that is invoked
-	 * when an action is not explicitly requested by users.
-	 */
-	public function actionIndex()
-	{
-		// renders the view file 'protected/views/site/index.php'
-		// using the default layout 'protected/views/layouts/main.php'
-		$this->render('index');
-	}
+    /**
+     * This is the default 'index' action that is invoked
+     * when an action is not explicitly requested by users.
+     * 
+     * @return void
+     */
+    public function actionIndex()
+    {
+        // renders the view file 'protected/views/site/index.php'
+        // using the default layout 'protected/views/layouts/main.php'
+        $this->render('index');
+    }
 
-	/**
-	 * This is the action to handle external exceptions.
-	 */
-	public function actionError()
-	{
-		if($error=Yii::app()->errorHandler->error)
-		{
-			if(Yii::app()->request->isAjaxRequest)
-				echo $error['message'];
-			else
-				$this->render('error', $error);
-		}
-	}
+    /**
+     * This is the action to handle external exceptions.
+     * 
+     * @return void
+     */
+    public function actionError()
+    {
+        if ($error = Yii::app()->errorHandler->error) {
+            if (Yii::app()->request->isAjaxRequest) {
+                echo $error['message'];
+            } else {
+                $this->render('error', $error);
+            }
+        }
+    }
 
-	/**
-	 * Displays the contact page
-	 */
-	public function actionContact()
-	{
-		$model=new ContactForm;
-		if(isset($_POST['ContactForm']))
-		{
-			$model->attributes=$_POST['ContactForm'];
-			if($model->validate())
-			{
-				$name='=?UTF-8?B?'.base64_encode($model->name).'?=';
-				$subject='=?UTF-8?B?'.base64_encode($model->subject).'?=';
-				$headers="From: $name <{$model->email}>\r\n".
-					"Reply-To: {$model->email}\r\n".
-					"MIME-Version: 1.0\r\n".
-					"Content-type: text/plain; charset=UTF-8";
+    /**
+     * Displays the login page
+     * 
+     * @return void
+     */
+    public function actionLogin()
+    {
+        $this->socialLogin();
+        $model = new LoginForm;
 
-				mail(Yii::app()->params['adminEmail'],$subject,$model->body,$headers);
-				Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
-				$this->refresh();
-			}
-		}
-		$this->render('contact',array('model'=>$model));
-	}
+        // if it is ajax validation request
+        if (Yii::app()->getRequest()->getPost('ajax') === 'login-form') {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
 
-	/**
-	 * Displays the login page
-	 */
-	public function actionLogin()
-	{
-		$this->socialLogin();
-		$model=new LoginForm;
+        // collect user input data
+        if (Yii::app()->getRequest()->getPost('LoginForm')) {
+            $model->attributes = Yii::app()->getRequest()->getPost('LoginForm');
+            // validate user input and redirect to the previous page if valid
+            if ($model->validate() && $model->login()) {
+                $this->redirect(Yii::app()->user->returnUrl);
+            }
+        }
+        // display the login form
+        $this->render('login', array('model' => $model));
+    }
 
-		// if it is ajax validation request
-		if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
+    /**
+     * Logs out the current user and redirect to homepage.
+     * 
+     * @return void
+     */
+    public function actionLogout()
+    {
+        Yii::app()->user->logout();
+        $this->redirect(Yii::app()->homeUrl);
+    }
 
-		// collect user input data
-		if(isset($_POST['LoginForm']))
-		{
-			$model->attributes=$_POST['LoginForm'];
-			// validate user input and redirect to the previous page if valid
-			if($model->validate() && $model->login())
-				$this->redirect(Yii::app()->user->returnUrl);
-		}
-		// display the login form
-		$this->render('login',array('model'=>$model));
-	}
-
-	/**
-	 * Logs out the current user and redirect to homepage.
-	 */
-	public function actionLogout()
-	{
-		Yii::app()->user->logout();
-		$this->redirect(Yii::app()->homeUrl);
-	}
-
-	/**
-	 * Social network login
-	 *
-	 * @return void
-	 */
-	public function socialLogin()
-	{
-		$service = Yii::app()->request->getQuery('service');
+    /**
+     * Social network login
+     *
+     * @return void
+     */
+    public function socialLogin()
+    {
+        $service = Yii::app()->request->getQuery('service');
         if (isset($service)) {
             $authIdentity = Yii::app()->eauth->getIdentity($service);
             $authIdentity->redirectUrl = Yii::app()->user->returnUrl;
@@ -126,15 +109,15 @@ class SiteController extends Controller
 
                 // successful authentication
                 if ($identity->authenticate()) {
-                	$userIdentity = User::model()->handleIdentity($identity);
-                	$userIdentity->setForceLogin(true);
-                	$userIdentity->authenticate();
+                    $socialUser = User::model()->createUserByIdentity($identity);
+                    $userIdentity = new UserIdentity($socialUser->email, $socialUser->password);
+                    $userIdentity->setForceLogin(true);
+                    $userIdentity->authenticate();
                     Yii::app()->user->login($userIdentity);
 
                     // special redirect with closing popup window
                     $authIdentity->redirect();
-                }
-                else {
+                } else {
                     // close popup window and redirect to cancelUrl
                     $authIdentity->cancel();
                 }
@@ -143,5 +126,5 @@ class SiteController extends Controller
             // Something went wrong, redirect to login page
             $this->redirect(array('site/login'));
         }
-	}
+    }
 }
--- quiz/protected/models/User.php
+++ quiz/protected/models/User.php
@@ -23,110 +23,121 @@ class User extends CActiveRecord
 {
     const ADMIN_ROLE = 'admin';
     const GUEST_ROLE = 'guest';
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
-	 * @return User the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'users';
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * 
+     * @param string $className active record class name.
+     * @return User the static model class
+     */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('name', 'required'),
-			array('role, is_oggetto', 'numerical', 'integerOnly'=>true),
-			array('name, email, photo_url, password, facebook_id, github_id, google_id, linkedin_id, twitter_id, vk_id, yandex_id', 'length', 'max'=>255),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('user_id, name, email, photo_url, role, password, is_oggetto, facebook_id, github_id, google_id, linkedin_id, twitter_id, vk_id, yandex_id', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+     * Entity table name
+     * 
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'users';
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-		);
-	}
+    /**
+     * Model validation rules
+     * 
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('name', 'required'),
+            array('role, is_oggetto', 'numerical', 'integerOnly' => true),
+            array('name, email, photo_url, password, facebook_id, github_id, google_id, linkedin_id, twitter_id, vk_id, yandex_id', 'length', 'max' => 255),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('user_id, name, email, photo_url, role, password, is_oggetto, facebook_id, github_id, google_id, linkedin_id, twitter_id, vk_id, yandex_id', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'user_id' => 'User',
-			'name' => 'Name',
-			'email' => 'Email',
-			'photo_url' => 'Photo Url',
-			'role' => 'Role',
-			'password' => 'Password',
-			'is_oggetto' => 'Is Oggetto',
-			'facebook_id' => 'Facebook',
-			'github_id' => 'Github',
-			'google_id' => 'Google',
-			'linkedin_id' => 'Linkedin',
-			'twitter_id' => 'Twitter',
-			'vk_id' => 'Vk',
-			'yandex_id' => 'Yandex',
-		);
-	}
+    /**
+     * User relations
+     * 
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+    /**
+     * Labels for User entity attributes
+     * 
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'user_id'     => 'User',
+            'name'        => 'Name',
+            'email'       => 'Email',
+            'photo_url'   => 'Photo Url',
+            'role'        => 'Role',
+            'password'    => 'Password',
+            'is_oggetto'  => 'Is Oggetto',
+            'facebook_id' => 'Facebook',
+            'github_id'   => 'Github',
+            'google_id'   => 'Google',
+            'linkedin_id' => 'Linkedin',
+            'twitter_id'  => 'Twitter',
+            'vk_id'       => 'Vk',
+            'yandex_id'   => 'Yandex',
+            );
+    }
 
-		$criteria=new CDbCriteria;
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * 
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
 
-		$criteria->compare('user_id',$this->user_id);
-		$criteria->compare('name',$this->name,true);
-		$criteria->compare('email',$this->email,true);
-		$criteria->compare('photo_url',$this->photo_url,true);
-		$criteria->compare('role',$this->role);
-		$criteria->compare('password',$this->password,true);
-		$criteria->compare('is_oggetto',$this->is_oggetto);
-		$criteria->compare('facebook_id',$this->facebook_id,true);
-		$criteria->compare('github_id',$this->github_id,true);
-		$criteria->compare('google_id',$this->google_id,true);
-		$criteria->compare('linkedin_id',$this->linkedin_id,true);
-		$criteria->compare('twitter_id',$this->twitter_id,true);
-		$criteria->compare('vk_id',$this->vk_id,true);
-		$criteria->compare('yandex_id',$this->yandex_id,true);
+        $criteria = new CDbCriteria;
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+        $criteria->compare('user_id', $this->user_id);
+        $criteria->compare('name', $this->name, true);
+        $criteria->compare('email', $this->email, true);
+        $criteria->compare('photo_url', $this->photo_url, true);
+        $criteria->compare('role', $this->role);
+        $criteria->compare('password', $this->password, true);
+        $criteria->compare('is_oggetto', $this->is_oggetto);
+        $criteria->compare('facebook_id', $this->facebook_id, true);
+        $criteria->compare('github_id', $this->github_id, true);
+        $criteria->compare('google_id', $this->google_id, true);
+        $criteria->compare('linkedin_id', $this->linkedin_id, true);
+        $criteria->compare('twitter_id', $this->twitter_id, true);
+        $criteria->compare('vk_id', $this->vk_id, true);
+        $criteria->compare('yandex_id', $this->yandex_id, true);
+
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
 
     /**
      * Retrieve random password
      *
-     * @param   int $length
+     * @param int $length password length
      * @return  string
      */
     public function generatePassword($length = 6)
@@ -134,70 +145,73 @@ class User extends CActiveRecord
         return substr(md5(uniqid(rand(), true)), 0, $length);
     }
 
-	/**
-	 * Check identity and create a new user if requested user cannot be found
-	 *
-	 * @param UserIdentity $identity identity
-	 *
-	 * @return User
-	 */
-	public function handleIdentity($identity)
-	{
-		$user = false;
-		if ($identity->getState('service_id')) {
-			$user = self::model()->findByAttributes(array($identity->getState('service_id') => $identity->getId()));
-		}
-		if (!$user && $identity->getState('email')) {
-			$user = self::model()->findByAttributes(array('email' => $identity->getState('email')));
-		}
-		if (!$user) {
-			$user = new self;
-			$user->name = $identity->getName();
-			$user->email = $identity->getState('email');
-			$user->photo_url = $identity->getState('photo');
-			$user->password = md5($this->generatePassword());
-			$user->is_oggetto = 0;
-		}
-
-		if ($serviceId = $identity->getState('service_id')) {
-			$user->$serviceId = $identity->getId();
-		}
-		$user->save();
-		$identity = new UserIdentity($user->email, $user->password);
-		return $identity;
-	}
-
-        public function getYandexUrl()
-        {
-            $url = 'http://api.moikrug.ru/v1/person?ids=' . $this->yandex_id;
-
-            $json = file_get_contents($url);
-            $yandex = json_decode($json);
-            return $yandex->link;
+    /**
+     * Check identity and create a new user if requested user cannot be found
+     *
+     * @param CBaseUserIdentity $identity identity
+     *
+     * @return User
+     */
+    public function createUserByIdentity(CBaseUserIdentity $identity)
+    {
+        
+        if (!($user = $this->loadUserByIdentity($identity))) {
+            $user = new self;
+            $user->name       = $identity->getName();
+            $user->email      = $identity->getState('email');
+            $user->photo_url  = $identity->getState('photo');
+            $user->password   = md5($this->generatePassword());
+            $user->is_oggetto = false;
         }
 
-        /**
-         * Get roles
-         *
-         * @return array
-         */
-        public static function getRoles()
-        {
-            return array(
-                self::GUEST_ROLE,
-                self::ADMIN_ROLE,
-            );
+        if ($serviceId = $identity->getState('service_id')) {
+            $user->$serviceId = $identity->getId();
         }
+        $user->save();
+        return $user;
+    }
 
-        /**
-         * Get role
-         *
-         * @param int $role id
-         * @return str
-         */
-        public static function getRole($role)
-        {
-            $array = self::getRoles();
-            return isset($array[$role])?$array[$role]:'';
+    /**
+     * Load User by User identity
+     *
+     * @param CBaseUserIdentity $identity user identity
+     *
+     * @return User|bool
+     */
+    public function loadUserByIdentity(CBaseUserIdentity $identity)
+    {
+        $user = false;
+        if ($identity->getState('service_id')) {
+            $user = self::model()->findByAttributes(array($identity->getState('service_id') => $identity->getId()));
         }
+        if (!$user && $identity->getState('email')) {
+            $user = self::model()->findByAttributes(array('email' => $identity->getState('email')));
+        }
+        return $user;
+    }
+
+    /**
+     * Get roles
+     *
+     * @return array
+     */
+    public static function getRoles()
+    {
+        return array(
+            self::GUEST_ROLE,
+            self::ADMIN_ROLE,
+        );
+    }
+
+    /**
+     * Get role
+     *
+     * @param int $role id
+     * @return str
+     */
+    public static function getRole($role)
+    {
+        $array = self::getRoles();
+        return isset($array[$role]) ?$array[$role] : '';
+    }
 }

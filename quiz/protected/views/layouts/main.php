96730335b7a39a5870ffa2dd84451ec9f4b6768d
Denis Obukhov <denis.obukhov@oggettoweb.com>
2013-02-08 10:35:08 +0400

    Change header font

--- quiz/protected/views/layouts/main.php
+++ quiz/protected/views/layouts/main.php
@@ -15,8 +15,6 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.min.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-	<link href='http://fonts.googleapis.com/css?family=Lobster&subset=latin,cyrillic-ext,latin-ext,cyrillic' rel='stylesheet' type='text/css'>
-
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
